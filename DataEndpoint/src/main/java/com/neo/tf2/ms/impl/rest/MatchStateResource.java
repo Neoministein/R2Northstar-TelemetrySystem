0d0fa0c25a831fb5ccb987de6f472cef371c1a87
Sandro Tartoni <sandro.tartoni@swislog.com>
2022-05-02 20:39:13 +0200

    reworked backend

--- DataEndpoint/src/main/java/com/neo/tf2/ms/impl/rest/MatchStateResource.java
+++ DataEndpoint/src/main/java/com/neo/tf2/ms/impl/rest/MatchStateResource.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.neo.common.impl.json.JsonSchemaUtil;
 import com.neo.common.impl.json.JsonUtil;
 import com.neo.javax.api.persitence.repository.SearchRepository;
+import com.neo.tf2.ms.impl.persistence.GlobalGameState;
 import com.neo.tf2.ms.impl.persistence.searchable.MatchState;
 import com.neo.util.javax.api.rest.RestAction;
 import com.neo.util.javax.impl.rest.AbstractRestEndpoint;
@@ -15,11 +16,10 @@ import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.UUID;
 
 @RequestScoped
 @Path(MatchStateResource.RESOURCE_LOCATION)
@@ -28,10 +28,13 @@ public class MatchStateResource extends AbstractRestEndpoint {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MatchStateResource.class);
 
-    public static final String RESOURCE_LOCATION = "api/v1/matchState";
+    public static final String RESOURCE_LOCATION = "api/v1/matchstate";
 
     @Inject
     SearchRepository searchRepository;
+
+    @Inject
+    GlobalGameState globalGameState;
 
     @PUT
     public Response put(String x) {
@@ -41,11 +44,23 @@ public class MatchStateResource extends AbstractRestEndpoint {
             JsonSchemaUtil.isValidOrThrow(jsonNode, MatchState.JSON_SCHEMA);
             MatchState matchState = new MatchState(jsonNode.deepCopy());
 
-            searchRepository.index(matchState);
+            //searchRepository.index(matchState);
+            globalGameState.setCurrentMatchState(jsonNode);
             return DefaultResponse.success(requestContext);
         };
 
         return super.restCall(restAction,requestContext);
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response get(@PathParam("id") String id) {
+        RequestContext requestContext = getContext(HttpMethod.GET,"/" + id);
+        RestAction restAction = () -> {
+            JsonNode state = globalGameState.getCurrentMatchState(UUID.fromString(id));
+            return DefaultResponse.success(requestContext, state);
+        };
+        return super.restCall(restAction, requestContext);
     }
 
     @Override
