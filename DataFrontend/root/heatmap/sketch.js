4c5a10aa4f9d7467091ff5ab80d5c725d5b2a121
Neoministein <acertartoni@gmail.com>
2022-07-05 22:07:39 +0200

    fixes json

--- DataFrontend/root/heatmap/sketch.js
+++ DataFrontend/root/heatmap/sketch.js
@@ -16,13 +16,13 @@ function draw() {
 	background(bg);
 	if(json) {
 		let highest = 5;
-		for (let i = 0; i < json.entries.length; i++) {
-			let c = color(json.entries[i].count / highest * 255, 255, 255);
+		for (let i = 0; i < json.data.entries.length; i++) {
+			let c = color(json.data.entries[i].count / highest * 255, 255, 255);
 
 			fill(c);
 			noStroke();
 			//set(json.entries[i].posX, json.entries[i].posY, c);
-			rect(json.entries[i].x, json.entries[i].y, 4, 4);
+			rect(json.data.entries[i].x, json.data.entries[i].y, 4, 4);
 		}
 	}
 }
