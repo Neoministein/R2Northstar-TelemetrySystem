682049fe447e2c6b680289296cbf78b04daae1ae
Neoministein <acertartoni@gmail.com>
2022-08-14 19:09:55 +0200

    fixed hibernate migration for custom Types

--- DataEndpoint/src/main/java/com/neo/r2/ts/impl/persistence/entity/mapper/JsonNodeStringJavaDescriptor.java
+++ DataEndpoint/src/main/java/com/neo/r2/ts/impl/persistence/entity/mapper/JsonNodeStringJavaDescriptor.java
@@ -3,54 +3,25 @@ package com.neo.r2.ts.impl.persistence.entity.mapper;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.neo.util.common.impl.exception.InternalJsonException;
 import com.neo.util.common.impl.json.JsonUtil;
-import org.hibernate.type.descriptor.WrapperOptions;
-import org.hibernate.type.descriptor.java.AbstractClassJavaType;
-
-public class JsonNodeStringJavaDescriptor extends AbstractClassJavaType<JsonNode> {
-
-    public static final JsonNodeStringJavaDescriptor INSTANCE = new JsonNodeStringJavaDescriptor();
-
-    protected JsonNodeStringJavaDescriptor() {
-        super(JsonNode.class);
-    }
-
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+@Converter
+public class JsonNodeStringJavaDescriptor implements AttributeConverter<JsonNode, String> {
     @Override
-    public String toString(JsonNode value) {
+    public String convertToDatabaseColumn(JsonNode jsonNode) {
         try {
-            return JsonUtil.toJson(value);
+            return JsonUtil.toJson(jsonNode);
         } catch (InternalJsonException e) {
-            throw new IllegalArgumentException("The given JsonNode object value: " + value + " cannot be transformed to a String", e);
+            throw new IllegalArgumentException("The given JsonNode object value: " + jsonNode + " cannot be transformed to a String", e);
         }
     }
 
-    public JsonNode fromString(String string) {
+    @Override
+    public JsonNode convertToEntityAttribute(String string) {
         try {
             return JsonUtil.fromJson(string);
         } catch (InternalJsonException e) {
             throw new IllegalArgumentException("The given string value: " + string + " cannot be transformed to JsonNode object", e);
         }
-    }
-
-    @Override
-    public <X> X unwrap(JsonNode value, Class<X> type, WrapperOptions options) {
-        if (value == null) {
-            return null;
-        }
-        if (String.class.isAssignableFrom(type)) {
-            return (X) toString(value);
-        }
-        throw unknownUnwrap(type);
-    }
-
-    @Override
-    public <X> JsonNode wrap(X value, WrapperOptions options) {
-        if (value == null) {
-            return null;
-        }
-        if (value instanceof String) {
-            return fromString(value.toString());
-        }
-
-        throw unknownWrap(value.getClass());
     }
 }
