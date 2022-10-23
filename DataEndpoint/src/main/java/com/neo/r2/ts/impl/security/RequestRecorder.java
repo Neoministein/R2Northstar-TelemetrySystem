282299026a3263a1876dc0339f7acfbd53270a09
Neoministein <acertartoni@gmail.com>
2022-10-23 23:36:15 +0200

    fixed RequestRecorder

--- DataEndpoint/src/main/java/com/neo/r2/ts/impl/security/RequestRecorder.java
+++ DataEndpoint/src/main/java/com/neo/r2/ts/impl/security/RequestRecorder.java
@@ -2,6 +2,7 @@ package com.neo.r2.ts.impl.security;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.neo.r2.ts.impl.persistence.searchable.RequestLog;
+import com.neo.util.common.impl.exception.InternalJsonException;
 import com.neo.util.common.impl.json.JsonUtil;
 import com.neo.util.framework.api.connection.RequestDetails;
 import com.neo.util.framework.api.persistence.search.SearchRepository;
@@ -39,15 +40,17 @@ public class RequestRecorder implements ContainerResponseFilter {
                 RequestDetails requestDetails = requestDetailsProvider.get();
                 HttpRequestDetails httpRequestDetails = (HttpRequestDetails) requestDetails;
 
-                JsonNode responseBody = JsonUtil.fromJson((String) containerResponse.getEntity());
+
+
+
                 RequestLog.RequestSegments requestSegment = new RequestLog.RequestSegments(
                         new Date(),
                         requestDetails.getRequestId(),
                         httpRequestDetails.getUser().isPresent() ? httpRequestDetails.getUser().get().getName() : "",
                         httpRequestDetails.getRemoteAddress(),
                         requestDetails.getRequestContext().toString(),
-                        responseBody.get("status").asText(),
-                        responseBody.has("error") ? responseBody.get("error").get("code").asText() : "",
+                        Integer.toString(containerResponse.getStatus()),
+                        getErrorCodeIfPresent(containerResponse),
                         System.currentTimeMillis() - requestDetails.getRequestStartDate().getTime(),
                         containerRequest.getHeaders().get("User-Agent") != null ? containerRequest.getHeaders().get("User-Agent").toString() : "");
                 if (searchRepository.enabled()) {
@@ -59,6 +62,23 @@ public class RequestRecorder implements ContainerResponseFilter {
                 LOGGER.warn("Unable to parse request segments [{}]", ex.getMessage());
             }
         }
+    }
+
+    protected String getErrorCodeIfPresent(ContainerResponseContext containerResponse) {
+        if (!containerResponse.hasEntity() || containerResponse.getStatus() == 200) {
+            return "";
+        }
+        try {
+            JsonNode responseBody = JsonUtil.fromJson((String) containerResponse.getEntity());
+            if (responseBody.has("code")) {
+                return responseBody.get("code").asText();
+            }
+        } catch (InternalJsonException ignored) {
+
+        } catch (Exception ex) {
+            LOGGER.warn("Unable to parse response body [{}]", ex.getMessage());
+        }
+        return "";
     }
 
     protected void searchResolver(RequestLog.RequestSegments requestSegments) {
