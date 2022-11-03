08474a691dd51f94e0a60d3a50bd9de67a3c06de
Sandro Tartoni <sandro.tartoni@swisslog.com>
2022-11-03 22:48:50 +0100

    cleaned up code

--- DataFrontend/src/components/Heatmap.tsx
+++ DataFrontend/src/components/Heatmap.tsx
@@ -7,7 +7,7 @@ export interface HeatmapProps {
     map: GameMapEntity
 }
 
-export default function Heatmap(props : HeatmapProps) {
+export default function Heatmap({heatmap, map} : HeatmapProps) {
 
     let canvasSize = getCanvasSize();
 
@@ -23,22 +23,22 @@ export default function Heatmap(props : HeatmapProps) {
         let bg: p5.Image
 
         p5Instance.preload = () => {
-            bg = p5Instance.loadImage('/img/map/' + props.map.name + '.png');
+            bg = p5Instance.loadImage('/img/map/' + map.name + '.png');
         }
         p5Instance.setup = () => p5Instance.createCanvas(canvasSize, canvasSize, p5Instance.P2D);
 
         p5Instance.draw = () => {
-            p5Instance.background(bg as p5.Image, 255 as number);
-            if(props.heatmap.data) {
+            p5Instance.background(bg, 255 as number);
+            if(heatmap.data) {
                 let highest = 25;
-                for (let i = 0; i < props.heatmap.data.entries.length; i++) {
-                    let c = p5Instance.color(props.heatmap.data.entries[i].count / highest * 255, 255, 255);
+                for (const element of heatmap.data.entries) {
+                    let c = p5Instance.color(element.count / highest * 255, 255, 255);
 
                     p5Instance.fill(c);
                     p5Instance.noStroke();
                     p5Instance.rect(
-                        scalePosition(props.heatmap.data.entries[i].x),
-                        scalePosition(props.heatmap.data.entries[i].y),
+                        scalePosition(element.x),
+                        scalePosition(element.y),
                         scalePosition(4),
                         scalePosition(4));
                 }
